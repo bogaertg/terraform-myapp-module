23bd97f5ba8407922d7b6ff91e6444ef8b452a0d
Gaetan BOGAERT <30294729+bogaertg@users.noreply.github.com>
2020-02-06 10:31:33 +0100

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -1,3 +1,7 @@
 module "myApp1" {
 source = "github.com/bogaertg/terraform-technical-module//technical-module1?ref=technical-module1-v1.0.0"
 }
+  
+module "myApp2" {
+source = "github.com/bogaertg/terraform-technical-module//technical-module2?ref=technical-module2-v1.0.0"
+}
